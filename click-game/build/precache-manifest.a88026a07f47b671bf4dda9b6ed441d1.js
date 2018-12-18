2cd0e95eceb823278e48b7e938c21124f1454c6e
Guomubai <erickjgarcia@gmail.com>
2018-12-17 18:45:19 -0700

    build uploaded

---
+++ click-game/build/precache-manifest.a88026a07f47b671bf4dda9b6ed441d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "368e5b63174c106c0780",
+    "url": "/static/js/main.368e5b63.chunk.js"
+  },
+  {
+    "revision": "3ad5f8a614c5e526e94e",
+    "url": "/static/js/1.3ad5f8a6.chunk.js"
+  },
+  {
+    "revision": "368e5b63174c106c0780",
+    "url": "/static/css/main.95ff0e0b.chunk.css"
+  },
+  {
+    "revision": "321c74d37df6365fcc61a427a617592c",
+    "url": "/index.html"
+  }
+];
